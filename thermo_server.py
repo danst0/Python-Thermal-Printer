e8f34c1aadc99c6cdc60b37575192132ed34b618
Daniel Dumke <ddumke@gmx.net>
2015-08-18 20:51:37 +0200

    Add class for web requests

--- thermo_server.py
+++ thermo_server.py
@@ -37,34 +37,27 @@ dailyFlag    = False # Set after daily trigger occurs
 lastId       = '1'   # State information passed to/from interval script
 
 
+class WebRequest():
+    def __init__(self, name):
+        self.app = Flask(name)
 
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   24 : {'name' : 'coffee maker', 'state' : GPIO.LOW},
-   25 : {'name' : 'lamp', 'state' : GPIO.LOW}
-   }
-
-# Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+    def run():
+        self.app.run(host='0.0.0.0', port=80, debug=True)
 
 @app.route("/")
-def main():
+    def main(self):
    # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
    # Put the pin dictionary into the template data dictionary:
-   templateData = {
-      'pins' : pins
-      }
-   # Pass the template data into the template main.html and return it to the user
+        templateData = {'pins': pins}
+        # Pass the template data into the template main.html and
+        # return it to the user
    return render_template('main.html', **templateData)
 
-# The function below is executed when someone requests a URL with secret_key, action and param
 @app.route("/<secret_key>/<action>/<param>")
-def action(secret_key, action, param):
+     def action(self, secret_key, action, param):
+         """Executed when someone requests a URL with secret_key, action and param"""
    if secret_key == config['api']['secret_key']:
       # key is correct, so continue
       if action == 'print':
